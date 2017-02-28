5c90befc290dcf84bc6b10da8a71f162b2fa8193
apd1997 <akash19dhanesha@gmail.com>
2017-02-28 14:27:23 +0530

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,10 @@
+var company = new Object();
+company.name = "Reliance";
+company.ceo = new Object();
+company.ceo.firstName = "Mukesh";
+console.log(company);
+console.log("Company CEO name is: " + company.ceo.firstName);
+console.log(company["name"]);
+var stockPropName = "stock of company";
+company[stockPropName] = 110;
+console.log("Stock price is: " + company[stockPropName]);
