4bf7d5fc29fdab41f99e98827b01e94887283b0c
apd1997 <akash19dhanesha@gmail.com>
2017-03-21 16:30:54 +0530

    Create m2_script.js

---
+++ m2_script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    
+    document.querySelector("button")
+      .addEventListener("click", function () {
+    var name =
+   document.getElementById("name").value; 
+   
+   document
+    .getElementById("content")
+    .innerHTML = message;
+  
+        $ajaxUtils
+          .sendGetRequest("data.json", 
+            function (res) {
+              if (res.Name==Akash Dhanesha) {
+                message += res.m1;
+                message+=res.m2;
+              }
+              else {
+                message += res.m3;
+                message += res.m4;
+              }
+              
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
+  }
+);
